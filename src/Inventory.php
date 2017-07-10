00efa48385e5075f531e571d617027971e383819
Max Scher and Steve Spitz <steve@>
2017-07-10 16:49:26 -0700

    non functioning tests added, bug hunting

--- src/Inventory.php
+++ src/Inventory.php
@@ -1,10 +1,28 @@
 <?php
-    class Inventory {
+    class Inventory
+    {
         private $item_name;
+        private $id;
 
-        function __constructor($item_name)
+        function __constructor($item_name, $id = null)
         {
             $this->item_name = $item_name;
+            $this->id = $id;
+        }
+
+        function setItem($new_item_name)
+        {
+            $this->item_name = (string) $new_item_name;
+        }
+
+        function getItem()
+        {
+            return $this->item_name;
+        }
+
+        function getId()
+        {
+            return $this->id;
         }
 
         function save()
@@ -20,23 +38,40 @@
 
         static function getAll()
         {
-
+            $returned_items = $GLOBALS['DB']->query("SELECT * FROM items;");
+            $items = array();
+            foreach($returned_items as $item) {
+                $item_name = $item['item_name'];
+                $id = $item['id'];
+                $new_item = new Inventory($item_name, $id);
+                array_push($items, $new_item);
+            }
+            return $items;
         }
 
         static function deleteAll()
         {
-
+            $executed = $GLOBALS['DB']->exec("DELETE FROM items;");
+            if ($executed) {
+               return true;
+            } else {
+               return false;
+            }
         }
 
-        static function find()
+        static function find($search_id)
         {
-
+            $returned_items = $GLOBALS['DB']->prepare("SELECT * FROM items WHERE id = :id");
+            $returned_items->bindParam(':id', $search_id, PDO::PARAM_STR);
+            $returned_items->execute();
+            foreach ($returned_items as $item) {
+               $item_name = $item['item_name'];
+               $item_id = $item['id'];
+               if ($item_id == $search_id) {
+                  $found_item = new Inventory($item_name, $item_id);
+               }
+            }
+            return $found_item;
         }
-
-        function getItem()
-        {
-            return $this->item_name;
-        }
-
     }
 ?>
--- tests/InventoryTest.php
+++ tests/InventoryTest.php
@@ -3,6 +3,7 @@
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
     */
+
     require_once "src/Inventory.php";
     $server = 'mysql:host=localhost:8889;dbname=inventory_test';
     $username = 'root';
@@ -11,20 +12,89 @@
 
     class Inventory_Test extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Task::deleteAll();
-        // }
+        protected function tearDown()
+        {
+            Inventory::deleteAll();
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $test_item_name = "Geronimo's Rifle";
+            $test_item = new Inventory($test_item_name);
+            $test_item->save();
+
+            //Act
+            $result = $test_item->getId();
+
+            //Assert
+            $this->assertTrue(is_numeric($result));
+        }
+
+        function testFind()
+        {
+            //Arrange
+            $test_item_name = "Monkey Bread";
+            $test_item_name_2 = "Coffee";
+            $test_item = new Inventory($test_item_name);
+            $test_item->save();
+            $test_item_2 = new Inventory($test_item_name_2);
+            $test_item_2->save();
+
+            //Act
+            $id = $test_item->getId();
+            $result = Inventory::find($id);
+
+            //Assert
+            $this->assertEquals($test_item, $result);
+        }
 
         function testSave()
         {
             //Arrange
-            $description = "Bob Ross";
-            $test_item = new Inventory($description);
+            $test_item_name = "Bob Ross";
+            $test_item = new Inventory($test_item_name);
+
             //Act
             $executed = $test_item->save();
+
             // Assert
             $this->assertTrue($executed, "Item not successfully saved to database");
+        }
+
+        function testGetAll()
+        {
+            //Arrange
+            $test_item_name = "Bob Ross";
+            $test_item_name_2 = "Shania Twain";
+            $test_item = new Inventory($test_item_name);
+            $test_item->save();
+            $test_item_2 = new Inventory($test_item_name_2);
+            $test_item_2->save();
+
+            //Act
+            $result = Inventory::getAll();
+
+            // Assert
+            $this->assertEquals([$test_item, $test_item_2], $result);
+        }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $item_name = "1912 Louisville Slugger";
+            $item_name_2 = "Ricky Ricardo's Microphone";
+            $test_item = new Inventory($item_name);
+            $test_item->save();
+            $test_item_2 = new Inventory($item_name_2);
+            $test_item_2->save();
+
+            //Act
+            Inventory::deleteAll();
+
+            //Assert
+            $result = Inventory::getAll();
+            $this->assertEquals([], $result);
         }
     }
 
